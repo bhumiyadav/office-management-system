1c60a71453f912f81787f00981e0fe72b03fb6a2
bhumiyadav <88817647+bhumiyadav@users.noreply.github.com>
2025-04-21 21:34:23 +0530

    urls.py

--- office_emp/emp_app/urls.py
+++ office_emp/emp_app/urls.py
@@ -1,13 +1,14 @@
-# from django.contrib import admin
 from django.urls import path
 from . import views
-
+ 
+app_name = 'emp_app'
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('view_all_emp/',views.all_emp,name='view_ll_emp'),
+    path('all_emp/',views.all_emp,name='all_emp'),
     path('add_emp/',views.add_emp,name='add_emp'),
     path('remove_emp/',views.remove_emp,name='remove_emp'),
-    path('filter_emp/',views.filter_emp,name='filter_emp'),
+    # path('filter_emp/',views.filter_emp,name='filter_emp'),
+    # path('register/',views.register, name='register'),
+    # path('login/',views.user_login,name='login'),  
 ]
