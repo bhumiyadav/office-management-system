231ff66994a0106150407ca305e2104d6f03858b
bhumiyadav <88817647+bhumiyadav@users.noreply.github.com>
2025-04-21 21:42:39 +0530

    models.py

--- office_emp/emp_app/models.py
+++ office_emp/emp_app/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 
-class department(models.Model):
+class Department(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
 
-class role(models.Model):
+class Role(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
        return self.name
 
-class employee(models.Model):
+class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    dept = models.ForeignKey(department, on_delete=models.CASCADE)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
     salary =models.IntegerField(default=0)
     bonus = models.IntegerField(default=0)
-    role = models.ForeignKey(role,on_delete=models.CASCADE)
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
     phone = models.IntegerField(default=0)
     hire_date =models.DateField()
     
